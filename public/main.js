0323a0f4864fb8d8fe9af3ae5aa9b38b3683c23e
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-13 14:55:28 -0500

    added a planning document and continued to broaden the scope

--- public/main.js
+++ public/main.js
@@ -1,16 +1,28 @@
+const mainURL = "http://localhost:5500/";
 const easterBtn = document.getElementById("is-it-easter");
 const midSection = document.getElementById("mid");
 
 const isItEaster = (event) => {
   event.preventDefault();
   console.log("hit");
-  addToList("It's Easter");
+  addToList(" It's Easter ");
 };
 
 const addToList = (script) => {
-  const dateAdded = document.createElement("p");
+  const dateAdded = document.createElement("li");
   dateAdded.textContent = script;
   midSection.appendChild(dateAdded);
 };
 
-easterBtn.addEventListener("click", isItEaster);
+const isItReallyEaster = (event) => {
+  event.preventDefault();
+  console.log("hit");
+  axios
+    .get(`${mainURL}easter`)
+    .then((res) => {
+      addToList(res.data);
+    })
+    .catch((err) => console.log(err));
+};
+
+easterBtn.addEventListener("click", isItReallyEaster);
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get("/easter", (req, res) => {
+  return "Yes it is";
+});
 
 app.listen(4000, console.log("Server running on 4000"));
