d542fc33d903b726ea9daa111750b560dac23ddb
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-16 14:47:36 -0500

    minor changes

--- server/server.js
+++ server/server.js
@@ -3,12 +3,10 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
-app.use(express.static("public"));
 
 const controllerList = require(`./controller.js`);
 
