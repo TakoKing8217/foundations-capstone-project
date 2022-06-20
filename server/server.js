aa8f92f3fa68dd8a01f1f9ba6e28dd27deebb1dc
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-20 10:44:43 -0500

    again

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json());
 app.use(express.static("public"));
+app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/main.html"));
+});
 
 const controllerList = require(`./controller.js`);
 
 const { lastDate, dateList, upcomingDates, deleteValue, getList } =
   controllerList;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/main.html"));
-});
 app.get("/what-season-now", lastDate);
 app.post("/get-date", dateList);
 app.get("/upcoming", upcomingDates);
